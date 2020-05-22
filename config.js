51a2af9fda3abe9dde029ba6a2e30f0042f77e22
Amos Lim <amoseui@gmail.com>
2020-05-22 23:52:27 +0900

    Fix deploy

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ module.exports = {
       telegram: '',
       twitter: '',
       github: 'amoseui',
-      rss: '',
+      rss: 'rss.xml',
       vkontakte: '',
       linkedin: '',
       instagram: '',
