962701548ee7d87d48408d42d5ae06f606f70a1b
Amos Lim <amoseui@gmail.com>
2023-01-16 23:19:14 +0900

    fix lint

--- src/components/Post/Comments/Utterances/Utterances.tsx
+++ src/components/Post/Comments/Utterances/Utterances.tsx
@@ -3,7 +3,7 @@ import React, { createRef, useLayoutEffect } from "react";
 import { useTheme } from "@/hooks";
 
 const src = "https://utteranc.es/client.js";
-const utterancesSelector = 'iframe.utterances-frame';
+const utterancesSelector = "iframe.utterances-frame";
 
 export interface IUtterancesProps {
   utterances: { [key: string]: string };
@@ -13,7 +13,7 @@ const Utterances: React.FC<IUtterancesProps> = React.memo(({ utterances }) => {
   const containerRef = createRef();
 
   const [{ mode }] = useTheme();
-  const themeMode = mode === "dark" ? "github-dark" : "github-light"
+  const themeMode = mode === "dark" ? "github-dark" : "github-light";
 
   useLayoutEffect(() => {
     const createUtterancesEl = () => {
@@ -36,11 +36,11 @@ const Utterances: React.FC<IUtterancesProps> = React.memo(({ utterances }) => {
       if (containerRef.current) {
         containerRef.current.appendChild(utterancesScript);
       }
-    }
+    };
 
     const postThemeMessage = () => {
       const message = {
-        type: 'set-theme',
+        type: "set-theme",
         theme: themeMode,
       };
       utterancesEl.contentWindow.postMessage(message, src);
